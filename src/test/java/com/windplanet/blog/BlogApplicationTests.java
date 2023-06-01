6fd68502b6ff5d91f2f1e64fae92423c05aa65ea
teicode <windplants@163.com>
2023-06-01 09:53:56 +0800

    init repo

---
+++ src/test/java/com/windplanet/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.windplanet.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
